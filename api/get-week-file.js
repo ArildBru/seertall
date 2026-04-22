16a9cd6f35a63491e6589d905a94edc20f7682db
ArildBru <arild.brubak@outlook.com>
2026-04-22 10:05:12 +0200

    Update get-week-file.js

--- api/get-week-file.js
+++ api/get-week-file.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   try {
     const { filename } = req.query;
 
-    const filePath = path.join(process.cwd(), "weeks", filename);
+    const filePath = path.join(process.cwd(), "data", "weeks", filename);
     const content = await readFile(filePath, "utf8");
 
     res.status(200).json({ data: JSON.parse(content) });
